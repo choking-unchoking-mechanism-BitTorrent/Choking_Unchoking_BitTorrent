4debe0a7e3e82112ea2a572860bcb6799ba0caff
SuperDrill <fshanheng@gmail.com>
2017-11-27 10:36:13 -0500

    Add send and receive in connection.

--- src/peer/Connection.java
+++ src/peer/Connection.java
@@ -1,15 +1,14 @@
 package peer;
 
-import analyzer.PeerInfo;
-import exception.*;
-import exception.message.HandshakeMessageException;
+import exception.message.MessageException;
 import message.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class Connection extends Thread {
     private Socket socket;
@@ -17,18 +16,35 @@ public class Connection extends Thread {
     private int myPeerID;
     private OutputStream outputStream;
     private InputStream inputStream;
-    private int downloadSpeed;
+    private boolean preferN;
+    private boolean opPrefer;
+    private boolean interested;
+    private boolean brocastHave;
+    private int lastReceive;
+    //Access peer process
+    private PeerProcess process;
+    private int downloadBytes;
 
-    public Connection(Socket socket, Peer peer, int myPeerID) {
+    public Connection(Socket socket, PeerProcess process, Peer peer, int myPeerID) {
         this.socket = socket;
         this.peer = peer;
+        this.process = process;
         this.myPeerID = myPeerID;
+        this.preferN = false;
+        this.opPrefer = false;
+        this.interested = false;
+        this.brocastHave = false;
+        downloadBytes = 0;
         try {
             this.outputStream = socket.getOutputStream();
             this.inputStream = socket.getInputStream();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setPreferN(boolean preferN){
+        this.preferN = preferN;
     }
 
     private boolean send(Message message) {
@@ -61,10 +77,14 @@ public class Connection extends Thread {
     }
     private boolean sendBitfield(){
         try{
-            outputStream.write(BitField.getBitFieldByteArray());
+            BitField field = process.getBitField(this.myPeerID);
+            outputStream.write(field.getBitFieldByteArray());
             outputStream.flush();
-            //wait for bitfield
-            //TODO
+            //wait for bit field
+            byte[] reply = new byte[field.getBitFieldByteArray().length];
+            BitField bitField = new BitField();
+            bitField.setBitField(reply);
+            process.addBitField(bitField, peer.getPeerId());
         }catch(Exception e){
             e.printStackTrace();
             return false;
@@ -72,9 +92,16 @@ public class Connection extends Thread {
         return true;
     }
 
-    private boolean sendPiece(){
-        //TODO
-        return false;
+    private boolean sendRequest(){
+        Message requestMessage = new Message(MessageConstant.REQUEST_LENGTH, MessageConstant.REQUEST_TYPE,
+                process.getRandomPieceIndex(peer.getPeerId()));
+        return send(requestMessage);
+    }
+
+    private boolean sendPiece(int pieceID){
+        Message pieceMessage = new Message(process.getPieceSize(), MessageConstant.PIECE_TYPE,
+                process.getFilePart(pieceID));
+        return send(pieceMessage);
     }
 
     private boolean sendInterested(){
@@ -96,7 +123,15 @@ public class Connection extends Thread {
         Message unchockedMsg = new Message(MessageConstant.UNCHOKE_LENGTH, MessageConstant.UNCHOKE_TYPE, null);
         return send(unchockedMsg);
     }
-
+    private boolean sendHave(){
+        Message sendMsg = new Message(MessageConstant.HAVE_LENGTH, MessageConstant.HAVE_TYPE,
+                ByteBuffer.allocate(4).putInt(lastReceive).array());
+        return send(sendMsg);
+    }
+    public void brocastHave(int received){
+        brocastHave = true;
+        lastReceive = received;
+    }
     @Override
     public void run() {
         try{
@@ -108,11 +143,96 @@ public class Connection extends Thread {
         }
         //Handshake first
         if (!handshake()){
-            //TODO
+            return;
         }
         //Send Bitfield
         if (!sendBitfield()){
+            return;
+        }
+        while (true){
+            if (process.ifAllPeersComplete())
+                break;
+            //Send
+            if (brocastHave){
+                sendHave();
+                brocastHave = false;
+            }
+            if (preferN){
+                //Send piece
+            }
+            if (opPrefer){
+                //Send piece
+            }
+            //receive
             //TODO
+            //read length first.
+            byte[] reply = new byte[4];
+            try{
+                int result = inputStream.read(reply);
+                //if data arrived
+                while(result == 4){
+                    int length = 0;
+                    for (int i = 0; i < 4; i++){
+                        length += reply[i] << (3-i);
+                    }
+                    byte[] type = new byte[1];
+                    result = inputStream.read(type);
+                    if (result != 1){
+                        throw new MessageException();
+                    }
+                    byte[] payload = new byte[length];
+                    result = inputStream.read(payload);
+                    if (result != length){
+                        throw new MessageException();
+                    }
+                    switch ((int)type[0]){
+                        case MessageConstant.UNCHOKE_TYPE:
+                            sendRequest();
+                            break;
+                        case MessageConstant.CHOKE_TYPE:
+                            //Do nothing
+                            break;
+                        case MessageConstant.HAVE_TYPE:
+                            //Update here
+                            int pieceNum1 = 0;
+                            for (int i = 0; i < 4; i++){
+                                pieceNum1 += reply[i] << (3-i);
+                            }
+                            process.updateBitField(pieceNum1, peer.getPeerId());
+                            break;
+                        case MessageConstant.INTERESTED_TYPE:
+                            process.updateInterestPeer(peer.getPeerId(), true);
+                            break;
+                        case MessageConstant.NOT_INTERESTED_TYPE:
+                            process.updateInterestPeer(peer.getPeerId(),false);
+                            break;
+                        case MessageConstant.PIECE_TYPE:
+                            //Get piece index
+                            int pieceNum2 = 0;
+                            for (int i = 0; i < 4; i++){
+                                pieceNum2 += reply[i] << (3-i);
+                            }
+                            //Update my bit field
+                            process.updateBitField(pieceNum2, peer.getPeerId());
+                            downloadBytes+=length;
+                            //Send piece to file
+                            process.writeIntoFile(Arrays.copyOfRange(reply, 5, reply.length), pieceNum2);
+                            break;
+                    }
+                    result = inputStream.read(reply);
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }catch (MessageException e){
+                e.printStackTrace();
+            }
+        }
+        try{
+            outputStream.close();
+            inputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            return;
         }
     }
 }
